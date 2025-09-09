b904d5ea2370568d9af0c8e128b0ea55f2437cfc
Jotham Teshome <jothamteshome@gmail.com>
2025-09-09 00:23:28 -0400

    Updating BACKEND_URL value in frontend to require a url and a port

--- frontend/src/services/room.ts
+++ frontend/src/services/room.ts
@@ -1,7 +1,7 @@
 // services/room.ts
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:4000";
+const BACKEND_URL = `${import.meta.env.REACT_APP_BACKEND_URL}:${import.meta.env.REACT_APP_BACKEND_PORT}`;
 
 export async function createRoom(videoId?: string) {
   const response = await axios.post(`${BACKEND_URL}/rooms/create-room`, { videoId });
