83812f7a4fd5063cc91b98172400d78582b2ba42
Jotham Teshome <jothamteshome@gmail.com>
2025-09-09 19:44:52 -0400

    removing port requirement from frontend url

--- frontend/src/services/room.ts
+++ frontend/src/services/room.ts
@@ -1,7 +1,7 @@
 // services/room.ts
 import axios from "axios";
 
-const BACKEND_URL = `${import.meta.env.VITE_APP_BACKEND_URL}:${import.meta.env.VITE_APP_BACKEND_PORT}`;
+const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 export async function createRoom(videoId?: string) {
   const response = await axios.post(`${BACKEND_URL}/api/v1/rooms/create-room`, { videoId });
