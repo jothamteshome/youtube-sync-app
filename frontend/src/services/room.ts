27921f941935543836fd2ab9ca62aa9863c86dff
Jotham Teshome <jothamteshome@gmail.com>
2025-09-09 17:39:55 -0400

    Updating frontend to handle backend routes properly

--- frontend/src/services/room.ts
+++ frontend/src/services/room.ts
@@ -1,9 +1,9 @@
 // services/room.ts
 import axios from "axios";
 
-const BACKEND_URL = `${import.meta.env.REACT_APP_BACKEND_URL}:${import.meta.env.REACT_APP_BACKEND_PORT}`;
+const BACKEND_URL = `${import.meta.env.VITE_APP_BACKEND_URL}:${import.meta.env.VITE_APP_BACKEND_PORT}`;
 
 export async function createRoom(videoId?: string) {
-  const response = await axios.post(`${BACKEND_URL}/rooms/create-room`, { videoId });
+  const response = await axios.post(`${BACKEND_URL}/api/v1/rooms/create-room`, { videoId });
   return response.data.roomId;
 }
